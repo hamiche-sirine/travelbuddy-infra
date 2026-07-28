1392aacfe3487a2f929553f21be7ed0dc432aef2
hamiche_sirine <hamichesirine@gmail.com>
2026-07-28 16:47:22 +0200

    ci: pipeline de validation terraform

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.project}-${var.environment}"
+  location = var.location
+
+  tags = {
+    project     = var.project
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+}
