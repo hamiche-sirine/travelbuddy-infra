81239b0eec9e01ccda722a328f1ae638b5954641
hamiche_sirine <hamichesirine@gmail.com>
2026-07-30 05:23:37 +0200

    chore: retention des journaux portee de 30 a 60 jours

--- container-apps.tf
+++ container-apps.tf
@@ -3,7 +3,7 @@ resource "azurerm_log_analytics_workspace" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = "PerGB2018"
-  retention_in_days   = 30
+  retention_in_days   = 60
 }
 
 resource "azurerm_container_app_environment" "main" {
